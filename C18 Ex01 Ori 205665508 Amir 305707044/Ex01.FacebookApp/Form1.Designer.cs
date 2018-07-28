680adf5dedc648d0a37a00238cc257da569a66f7
lermanori <lermanori@gmail.com>
2018-07-28 17:24:38 +0300

    posting login and logout
    logout has probleoms and naming inside form is also problematic

--- C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.Designer.cs
+++ C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.Designer.cs
@@ -32,13 +32,17 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(1248, 46);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "login";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,25 +67,55 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(561, 132);
+            this.button2.Location = new System.Drawing.Point(1248, 90);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 60);
+            this.button2.Size = new System.Drawing.Size(75, 37);
             this.button2.TabIndex = 3;
-            this.button2.Text = "load events";
+            this.button2.Text = "log out";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(1088, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 121);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(813, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(219, 38);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "post";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(813, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(219, 22);
+            this.textBox2.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1435, 699);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +127,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.cs
+++ C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.cs
@@ -9,13 +9,16 @@ using System.Windows.Forms;
 using FacebookWrapper;
 using System.Net;
 using System.IO;
+using System.Net;
 
 namespace Ex01.FacebookApp
 {
     public partial class Form1 : Form
     {
         LoginResult m_loginResult;
+        Action logout;
         FacebookWrapper.ObjectModel.User m_currentUser;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,31 +27,31 @@ namespace Ex01.FacebookApp
         private void button1_Click(object sender, EventArgs e)
         {
             login();
-
         }
+
         private void login()
         {
             m_loginResult = FacebookService.Login("273882356720887", "email", "user_hometown", "user_birthday", "user_friends", "user_events");
             textBox1.Text = m_loginResult.AccessToken.ToString();
             m_currentUser = m_loginResult.LoggedInUser;
+            pictureBox1.Image = m_currentUser.ImageNormal;
         }
-        private void loadEvents()
+
+        private void postStatus(string post)
         {
-
-            //FacebookWrapper.ObjectModel.FacebookObjectCollection<FacebookWrapper.ObjectModel.Event> myEvents = m_currentUser.Events;
-            try
-            {
-
-                foreach (FacebookWrapper.ObjectModel.Event eventush in m_currentUser.Events)
-                {
-                    listBox1.Items.Add(eventush);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString());
-            }
+            m_currentUser.PostStatus(post);
         }
+      
+        //somethings wrong here
+        public void on_logOut()
+        {
+            logout = on_logOut;
+            FacebookService.Logout(logout);
+            textBox1.Text = "";
+            pictureBox1.Image = null;
+        }
+
+       
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -57,13 +60,13 @@ namespace Ex01.FacebookApp
 
         private void button2_Click(object sender, EventArgs e)
         {
-            var request = (HttpWebRequest)WebRequest.Create("https://graph.facebook.com/v3.0/me?fields=events&access_token=EAAD5GDSVJPcBAE0wxuB2MrP8XUUnUlpXhrxdHGmzMvYkbP5n2VZBSfdXIErSN7ZAP8QhJn0lnRZBWvdR6r90TbvKQdX6OF9U9XDyXA2nhA0SdZAWuw48ievDAqZCUhAs3XwZA55INVkEvt6VqOeqYvpyzEcjz070oddpRfyhFj8AiaSSZCsL7VoBN59eefl5A4ZD");
+            on_logOut();
+        }
 
-            var response = (HttpWebResponse)request.GetResponse();
-
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-
-            MessageBox.Show(responseString);
+        private void button3_Click(object sender, EventArgs e)
+        {
+            postStatus(textBox2.Text);
+            textBox2.Text = "";
         }
     }
 }
