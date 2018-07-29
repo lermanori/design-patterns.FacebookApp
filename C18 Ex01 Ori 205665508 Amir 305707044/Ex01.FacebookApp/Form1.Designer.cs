5ce76ddb9de7b88b08dd0ac5a544c460b5eefbd8
lermanori <lermanori@gmail.com>
2018-07-29 16:41:03 +0300

    skeleton

--- C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.Designer.cs
+++ C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,9 +53,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(21, 33);
+            this.textBox1.Location = new System.Drawing.Point(21, 46);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(549, 22);
+            this.textBox1.Size = new System.Drawing.Size(524, 22);
             this.textBox1.TabIndex = 1;
             // 
             // listBox1
@@ -90,22 +93,51 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(219, 38);
             this.button3.TabIndex = 5;
-            this.button3.Text = "post";
+            this.button3.Text = "post status";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(813, 123);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.textBox2.Location = new System.Drawing.Point(813, 108);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(219, 22);
+            this.textBox2.Size = new System.Drawing.Size(219, 59);
             this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "enter post here";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(577, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1144, 261);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 36);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1435, 699);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
@@ -130,6 +162,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.cs
+++ C18 Ex01 Ori 205665508 Amir 305707044/Ex01.FacebookApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
 using System.Net;
 using System.IO;
 using System.Net;
@@ -31,7 +32,7 @@ namespace Ex01.FacebookApp
 
         private void login()
         {
-            m_loginResult = FacebookService.Login("273882356720887", "email", "user_hometown", "user_birthday", "user_friends", "user_events");
+            m_loginResult = FacebookService.Login("273882356720887", "email", "user_hometown", "user_birthday", "user_friends", "user_events", "groups_access_member_info");
             textBox1.Text = m_loginResult.AccessToken.ToString();
             m_currentUser = m_loginResult.LoggedInUser;
             pictureBox1.Image = m_currentUser.ImageNormal;
@@ -41,7 +42,7 @@ namespace Ex01.FacebookApp
         {
             m_currentUser.PostStatus(post);
         }
-      
+
         //somethings wrong here
         public void on_logOut()
         {
@@ -51,7 +52,7 @@ namespace Ex01.FacebookApp
             pictureBox1.Image = null;
         }
 
-       
+
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -66,7 +67,40 @@ namespace Ex01.FacebookApp
         private void button3_Click(object sender, EventArgs e)
         {
             postStatus(textBox2.Text);
-            textBox2.Text = "";
+            textBox2.Text = "enter post here";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            m_currentUser.ReFetch(DynamicWrapper.eLoadOptions.Full);
+            FacebookObjectCollection<Group> groupCollection = m_currentUser.Groups;
+
+            foreach (Group g in groupCollection)
+            {
+                listBox1.Items.Add(g.Name.ToString());
+            }
+
+            if(listBox1.Items.Count == 0)
+            {
+                listBox1.Items.Add("no items to show");
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show(openFileDialog1.FileName);
+            }
+            m_currentUser.PostPhoto()
         }
     }
 }
